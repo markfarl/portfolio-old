39a284847b97f48f21f8ac886549256bfe984c0b
MarkF <markfarl@gmail.com>
2024-09-30 16:53:59 +0100

    main

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Router from './Router'
+// import Router from './Router'
 import { DarkModeProvider } from "./contexts/DarkModeContext"
+import Portfolio from './pages/Portfolio'
+
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <DarkModeProvider>
-      <Router />
+      <Portfolio />
     </DarkModeProvider>
   </StrictMode>,
 )
